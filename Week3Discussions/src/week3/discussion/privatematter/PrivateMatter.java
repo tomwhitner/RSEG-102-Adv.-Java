9b08266aaea306b91c4326a21bc82a24d33c6f85
Tom Whitner <whitner.dev@gmail.com>
2012-02-01 16:54:43 -0500

    Added private matter disc answer

---
+++ Week3Discussions/src/week3/discussion/privatematter/PrivateMatter.java
@@ -0,0 +1,25 @@
+package week3.discussion.privatematter;
+
+class Base {
+	public String name = "Base";
+	public String getName() { return name + " (m)"; }
+}
+
+class Derived extends Base {
+	String name = "Derived";
+	public String getName() { return name + " (m)"; }
+}
+
+public class PrivateMatter {
+	public static void main(String[] args) {
+		System.out.println("Derived: " + new Derived().name);
+		
+		Base b = new Derived();
+		System.out.println("Base:    " + b.name);
+		System.out.println("Base:    " + b.getName());
+		
+		Derived d = new Derived();
+		System.out.println("Derived: " + d.name);
+		System.out.println("Derived: " + d.getName());
+	}
+}
