7d98df16f5fbc2102e2c12f365998386405d6207
Tom Whitner <whitner.dev@gmail.com>
2012-02-03 07:14:15 -0500

    added diference to ouput

--- Assignment1/src/tom/exceptions/TestDriver.java
+++ Assignment1/src/tom/exceptions/TestDriver.java
@@ -8,8 +8,7 @@ public class TestDriver {
 	/**
 	 * Test the exception classes
 	 * 
-	 * @param args
-	 *            Command line arguments; not used.
+	 * @param args Command line arguments; not used.
 	 */
 	public static void main(String[] args) {
 
@@ -20,20 +19,35 @@ public class TestDriver {
 		testWages[3] = new BigDecimal("6");
 		testWages[4] = new BigDecimal("100");
 		testWages[5] = new BigDecimal("199");
-		testWages[6] = new BigDecimal("200");  // added to test upper limit condition
+		testWages[6] = new BigDecimal("200"); // added to test upper limit
+												// condition
 		testWages[7] = new BigDecimal("250");
 
 		for (BigDecimal wage : testWages) {
 			try {
-				System.out.println("Validating wage: " + CURRENCY_FORMAT.format(wage));				
+				System.out.println("Validating wage: "
+						+ CURRENCY_FORMAT.format(wage));
 				verifyEmployeeWage(wage);
 				System.out.println(" Wage is valid.");
 			} catch (IllegalWageException e) {
-				System.out.println(" Wage " + CURRENCY_FORMAT.format(e.getWage()) + " is illegal.  Cannot be negative or zero.");
+				System.out.println(" Wage "
+						+ CURRENCY_FORMAT.format(e.getWage())
+						+ " is illegal.  Cannot be negative or zero.");
 			} catch (MinWageException e) {
-				System.out.println(" Wage " + CURRENCY_FORMAT.format(e.getWage()) + " is out of range.  Must be greater than or equal to " + CURRENCY_FORMAT.format(e.getWageLimit()));
+				System.out
+						.println(" Wage "
+								+ CURRENCY_FORMAT.format(e.getWage())
+								+ " is out of range.  Must be greater than or equal to "
+								+ CURRENCY_FORMAT.format(e.getWageLimit())
+								+ ".  Difference = "
+								+ CURRENCY_FORMAT.format(e.getWageDiff()) + ".");
 			} catch (MaxWageException e) {
-				System.out.println(" Wage " + CURRENCY_FORMAT.format(e.getWage()) + " is out of range.  Must be less than or equal to " + CURRENCY_FORMAT.format(e.getWageLimit()));
+				System.out.println(" Wage "
+						+ CURRENCY_FORMAT.format(e.getWage())
+						+ " is out of range.  Must be less than or equal to "
+						+ CURRENCY_FORMAT.format(e.getWageLimit())
+						+ ".  Difference = "
+						+ CURRENCY_FORMAT.format(e.getWageDiff()) + ".");
 			} finally {
 				System.out.println(" Wage validation complete.");
 				System.out.println("");
@@ -41,7 +55,8 @@ public class TestDriver {
 		}
 	}
 
-	private static final NumberFormat CURRENCY_FORMAT = NumberFormat.getCurrencyInstance();
+	private static final NumberFormat CURRENCY_FORMAT = NumberFormat
+			.getCurrencyInstance();
 	private static final BigDecimal LOWER_WAGE_LIMIT = new BigDecimal("6.00");
 	private static final BigDecimal UPPER_WAGE_LIMIT = new BigDecimal("200.00");
 
