0a87563caad6bb4d8ad58a187294aa08cff241e7
Tom Whitner <whitner.dev@gmail.com>
2012-02-08 16:51:48 -0500

    initial week 4 discussion ? answered.

---
+++ Week4Discussions/src/wwek3/discussions/indecisive/Indecisive.java
@@ -0,0 +1,17 @@
+package wwek3.discussions.indecisive;
+
+class Indecisive {
+
+	public static void main(String[] args) {
+		System.out.println(decision());
+	}
+
+	static boolean decision() {
+
+		try {
+			return true;
+		} finally {
+			return false;
+		}
+	}
+}
