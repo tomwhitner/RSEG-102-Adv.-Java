028e8ad54429645851d8d54078495a99553173ca
Tom Whitner <whitner.dev@gmail.com>
2012-02-23 09:34:21 -0500

    added basic ops example

---
+++ Week6Discussions/src/week6/discussions/basicOps/Trivial.java
@@ -0,0 +1,20 @@
+package week6.discussions.basicOps;
+
+public class Trivial {
+
+	public static void main(String args[]) {
+
+		System.out.print("H" + "a");
+
+		System.out.print('H' + 'a');
+
+		int H = 'H';
+		int a = 'a';
+		System.out.println("H = " + H);
+		System.out.println("a = " + a);
+		System.out.println("H + a = " + (H+a));
+		
+	}
+	
+
+}
