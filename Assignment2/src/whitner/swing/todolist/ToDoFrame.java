e46f8f66faa97a79e999a71626d06b6128849018
Tom Whitner <whitner.dev@gmail.com>
2012-02-23 10:25:56 -0500

    added panel for text, combo, and buttons.

--- Assignment2/src/whitner/swing/todolist/ToDoFrame.java
+++ Assignment2/src/whitner/swing/todolist/ToDoFrame.java
@@ -1,16 +1,19 @@
 package whitner.swing.todolist;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JTree;
+import javax.swing.border.Border;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class ToDoFrame extends JFrame {
@@ -27,12 +30,39 @@ public class ToDoFrame extends JFrame {
 
 		createTree();
 		
-		//createPanel();
+		createButtonPanel();
+
 	}
 	
+	private void createButtonPanel() {
+		JPanel panel = new JPanel();
+		
+		
+		JLabel taskLabel = new JLabel("Task");
+		panel.add(taskLabel);
+		JTextField taskTextField = new JTextField(20);
+		panel.add(taskTextField);
+		
+		JLabel priorityLabel = new JLabel("Priority");
+		panel.add(priorityLabel);
+		JComboBox priorityComboBox = new JComboBox();
+		for (int i = 1; i <= numberOfPriorities; i++) {
+			priorityComboBox.addItem(i);
+		}
+		panel.add(priorityComboBox);
 
-	
-	
+		JButton addButton = new JButton("Add");
+		panel.add(addButton);
+		JButton updateButton = new JButton("Update");
+		panel.add(updateButton);
+		JButton deleteButton = new JButton("Delete");
+		panel.add(deleteButton);
+		
+		Border border = BorderFactory.createEtchedBorder();
+		panel.setBorder(border);
+		
+		this.add(panel, BorderLayout.SOUTH);
+	}
 
 	private final int numberOfPriorities = 10;
 	private final DefaultMutableTreeNode root = new DefaultMutableTreeNode("Tasks");
@@ -48,7 +78,7 @@ public class ToDoFrame extends JFrame {
 
 		JTree tree = new JTree(root);
 		JScrollPane scrollPane = new JScrollPane(tree);
-		this.add(scrollPane, BorderLayout.NORTH);
+		this.add(scrollPane, BorderLayout.CENTER);
 	}
 
 	public static final int DEFAULT_WIDTH = 300;
