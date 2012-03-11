0238c17e793da7b884c5a3736e89ba66647abc98
Tom Whitner <whitner.dev@gmail.com>
2012-03-10 19:23:13 -0500

    Changed to IdentityHashMap

--- Week8Discussions/src/week8/discussions/names/Names.java
+++ Week8Discussions/src/week8/discussions/names/Names.java
@@ -1,32 +1,33 @@
 package week8.discussions.names;
 
 import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
-public class Names { 
+public class Names {
 
-	   private Map<String, String> m =new HashMap<String, String>(); 
+	private Map<String, String> m = new IdentityHashMap<String, String>();
 
-	   public Names() { 
+	public Names() {
 
-	      m.put("Mickey","Mouse"); 
+		m.put("Mickey", "Mouse");
 
-	      m.put("Mickey","Mantle"); 
-
-	   }
-
-	   public int size() {
-
-	      return m.size();
-
-	   }
-
-	   public static void main(String args []) { 
-
-	      Names names =new Names(); 
-
-	      System.out.println(names.size()); 
-
-	   } 
+		m.put("Mickey", "Mantle");
 
 	}
+
+	public int size() {
+
+		return m.size();
+
+	}
+
+	public static void main(String args[]) {
+
+		Names names = new Names();
+
+		System.out.println(names.size());
+
+	}
+
+}
