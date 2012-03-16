2dab52911e4f651862f805ed35d58687971396b6
Tom Whitner <whitner.dev@gmail.com>
2012-03-16 00:34:20 -0400

    more revisions.  getting closer!  implemented open/closed state.

--- FinalExam/src/tom/networking/client/FTClient.java
+++ FinalExam/src/tom/networking/client/FTClient.java
@@ -32,6 +32,7 @@ public class FTClient {
 	private Socket socket = null;
 	
 	private static final int PORT = 8189;
+	private static final int DATA_PORT = 8190;
 
 	public static void main(String[] args) {
 		new FTClient().run();
@@ -42,17 +43,14 @@ public class FTClient {
 		screenIn = new BufferedReader(new InputStreamReader(System.in));
 		screenOut = new PrintWriter(System.out, true /* autoFlush */);
 		
-		commands.put(Command.UNKNOWN, new UnknownCommand());
-		commands.put(Command.GET, new GetCommand());
-		commands.put(Command.OPEN, new OpenCommand());
-		commands.put(Command.QUIT, new QuitCommand());
-		commands.put(Command.CLOSE, new CloseCommand());
-
-		boolean proceed = true;
-		String line;
-
+		// configure commands based on closed state
+		configureCommands(false);
+		
 		try {
 			
+			boolean proceed = true;
+			String line;
+
 			screenOut.println("Welcome to FTClient...");
 			
 			while (((line = screenIn.readLine()) != null) && proceed) {
@@ -71,41 +69,90 @@ public class FTClient {
 
 	}
 	
-	private int sendCommandToServer(String command, String ... parameters) {
-		
-		String line = command;
-		for (String p : parameters) {
-			line = line + " " + p;
-		}
-		serverOut.println(line);
-		
-		String ret;
-		do {
-			ret = serverIn.nextLine();
-			screenOut.println(ret);
-		} while (ret.charAt(3) != ' ');
-		
-		int result = Integer.parseInt(ret.substring(0, 3));
-		return result;
-	}
-
+	/*
+	 * Get the specified command object form the map
+	 */
 	private Command getCommand(String commandName) {
+		
+		// attempt to retrieve command from map
 		Command cmd = commands.get(commandName.toUpperCase());
+		
+		// if no command is found, return the special unknown command
 		if (cmd == null) {
 			cmd = commands.get(Command.UNKNOWN);
 		}
+		
 		return cmd;
 	}
 	
-	class UnknownCommand implements Command {
-
-		@Override
-		public boolean execute(String[] parameters) {
-			screenOut.println("Unknown command: " + parameters[0]);
-			return true;
+	/*
+	 * Sends a command to the server and parses the response code
+	 */
+	private int sendCommandToServer(String command, String ... parameters) {
+		
+		// assemble the command line with parameters
+		String cmdLine = command;
+		for (String p : parameters) {
+			cmdLine = cmdLine + " " + p;
 		}
+		
+		// send the command line to the server
+		serverOut.println(cmdLine);
+		
+		// output each result line from the server
+		String restultLine;
+		do {
+			restultLine = serverIn.nextLine();
+			screenOut.println(restultLine);
+		} while (restultLine.charAt(3) != ' ');
+		
+		// parse the result code from the final result line
+		int resultCode = Integer.parseInt(restultLine.substring(0, 3));
+		
+		return resultCode;
 	}
 	
+	// optimzation, create each command only once
+	private final OpenCommand OPEN_COMMAND = new OpenCommand();
+	private final QuitCommand QUIT_COMMAND = new QuitCommand();
+	private final CloseCommand CLOSE_COMMAND = new CloseCommand();
+	private final KillCommand KILL_COMMAND = new KillCommand();
+	private final GetCommand GET_COMMAND = new GetCommand();
+	private final PutCommand PUT_COMMAND = new PutCommand();
+	private final ModeCommand MODE_COMMAND = new ModeCommand();
+	private final InvalidCommand INVALID_COMMAND = new InvalidCommand();
+	private final UnknownCommand UNKNOWN_COMMAND = new UnknownCommand();
+	
+	/*
+	 * Configure commands based on state of connection
+	 */
+	private void configureCommands(boolean open) {
+		
+		if (open) {
+			// configure commands based on open state
+			commands.put(Command.OPEN, INVALID_COMMAND);
+			commands.put(Command.QUIT, QUIT_COMMAND);
+			commands.put(Command.CLOSE, CLOSE_COMMAND);
+			commands.put(Command.KILL, KILL_COMMAND);
+			commands.put(Command.GET, GET_COMMAND);
+			commands.put(Command.PUT, PUT_COMMAND);
+			commands.put(Command.MODE, MODE_COMMAND);			
+			commands.put(Command.UNKNOWN, UNKNOWN_COMMAND);
+		} else {
+			// configure commands based on closed state
+			commands.put(Command.OPEN, OPEN_COMMAND);
+			commands.put(Command.QUIT, QUIT_COMMAND);
+			commands.put(Command.CLOSE, INVALID_COMMAND);
+			commands.put(Command.KILL, INVALID_COMMAND);
+			commands.put(Command.GET, INVALID_COMMAND);
+			commands.put(Command.PUT, INVALID_COMMAND);
+			commands.put(Command.MODE, INVALID_COMMAND);
+			commands.put(Command.UNKNOWN, UNKNOWN_COMMAND);
+		}
+		
+	}
+	
+
 	class OpenCommand implements Command {
 
 		@Override
@@ -127,6 +174,7 @@ public class FTClient {
 					// welcome
 					screenOut.println(serverIn.nextLine());
 					
+					// user
 					screenOut.println("User:");
 					String user = screenIn.readLine();
 					sendCommandToServer(tom.networking.server.Command.USER, user);
@@ -136,7 +184,9 @@ public class FTClient {
 					String pwd = screenIn.readLine();
 					int result = sendCommandToServer(tom.networking.server.Command.PASS, pwd);
 					
-					if (result == 200) {
+					// if login was successful
+					if (result == 200) {						
+						configureCommands(true);
 						screenOut.println("Connected to " + host);
 					} else {
 						screenOut.println("Failed to connect to " + host);
@@ -145,10 +195,8 @@ public class FTClient {
 				} catch (UnknownHostException e) {
 					screenOut.println("Unknown host: " + host);
 				} catch (ConnectException ex) {
-					screenOut.println("Connection to " + host + " refused");
-				}
-				catch (IOException e) {
-
+					screenOut.println("Connection refused: " + host);
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			
@@ -160,23 +208,6 @@ public class FTClient {
 		}
 	}
 	
-	class QuitCommand implements Command {
-
-		@Override
-		public boolean execute(String[] parameters) {
-			
-			if ((socket != null)  && (!socket.isClosed())) {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			return false;
-		}
-	}
-	
 	class CloseCommand implements Command {
 
 		@Override
@@ -184,7 +215,15 @@ public class FTClient {
 			
 			if ((socket != null)  && (!socket.isClosed())) {
 				try {
+					
+					sendCommandToServer(tom.networking.server.Command.QUIT);
+					
 					socket.close();
+					
+					configureCommands(false);
+					
+					screenOut.println("Connnection closed.");
+					
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -193,7 +232,27 @@ public class FTClient {
 			return true;
 		}
 	}
+	
+	class QuitCommand implements Command {
 
+		@Override
+		public boolean execute(String[] parameters) {
+			
+			// get the close command
+			Command cmd = getCommand(Command.CLOSE);
+			
+			// if the close command is acutally a close command, execute it
+			if (cmd.getClass() == CloseCommand.class) {
+				cmd.execute(parameters);
+			}
+			
+			screenOut.println("Quitting.  Goodbye.");
+
+			// tell main loop to terminate
+			return false;
+		}
+	}
+	
 	class GetCommand implements Command {
 
 		@Override
@@ -232,5 +291,56 @@ public class FTClient {
 		}
 
 	}
+	
+	class PutCommand implements Command {
+
+		@Override
+		public boolean execute(String[] parameters) {
+
+			screenOut.println("This command is not implemented yet.");
+			return true;
+		}
+	}
+	
+	class ModeCommand implements Command {
+
+		@Override
+		public boolean execute(String[] parameters) {
+
+			sendCommandToServer(tom.networking.server.Command.TYPE, parameters[1]);
+
+			return true;
+		}
+	}
+	
+	class KillCommand implements Command {
+
+		@Override
+		public boolean execute(String[] parameters) {
+
+			screenOut.println("This command is not implemented yet.");
+			return true;
+		}
+	}
+	
+	class InvalidCommand implements Command {
+
+		@Override
+		public boolean execute(String[] parameters) {
+			screenOut.println("This command is not valid in the current state.");
+			return true;
+		}
+	}
+	
+	class UnknownCommand implements Command {
+
+		@Override
+		public boolean execute(String[] parameters) {
+			screenOut.println("Unknown command: " + parameters[0]);
+			return true;
+		}
+	}
+	
+
 
 }
