1561574a66804e2d4441069b6611aa84162337c5
Tom Whitner <whitner.dev@gmail.com>
2012-03-18 21:51:24 -0400

    commenting

--- FinalExam/src/tom/networking/Command.java
+++ FinalExam/src/tom/networking/Command.java
@@ -1,7 +1,14 @@
 package tom.networking;
 
+/*
+ * Command interface to implement basic command pattern
+ */
 public interface Command {
 
+	/*
+	 * Execute the specific command.
+	 * Return true if process should continue; false if process should terminate.
+	 */
 	boolean execute(String[] parameters);
 
 }
